e9954fecb96c38d49459645195656fc6a78220e8
Jhonnatan Hernandez <136657501+Jhon68h@users.noreply.github.com>
2025-05-21 17:23:45 -0500

    Revert "se implemetó FilesController, DTO y repositorio para el manejo de arc…"

--- src/main/java/com/chat/socket/ms_chat_socket/Repository/FilesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chat.socket.ms_chat_socket.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.chat.socket.ms_chat_socket.Entity.Files;
-
-public interface FilesRepository extends JpaRepository<Files, Long> {
-    
-}
